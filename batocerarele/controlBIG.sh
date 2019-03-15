22384a5a34ae15c1553aea55bd63299afcf6b539
m41k <maik.alberto@hotmail.com>
2019-03-15 10:53:53 -0300

    Add files via upload

---
+++ batocerarele/controlBIG.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+echo -e "Content-type: text/html\n\n"
+
+#NBTS=4
+EQ[1]="LAMPADA"
+EQ[2]="VENTILADOR"
+EQ[3]="LED"
+EQ[4]="UMIDIFICADOR"
+
+
+for i in $(seq ${#EQ[@]}); do
+ if [[ $(cat /tmp/www/cgi-bin/t$i.txt) -eq 0 ]]; then
+  C[$i]="checked"
+  else
+  C[$i]=" "
+ fi
+ if [[ ${EQ[i]} == "" ]]; then
+  C[$i]="disabled"
+ fi
+
+done
+
+_CSS(){
+
+cat <<EOF
+
+<style>
+body {
+    background: #eee;
+}
+.toggle {
+    margin-bottom: 40px;
+}
+.toggle > input {
+    display: none;
+}
+.toggle > label {
+    position: relative;
+    display: block;
+    height: 80px;
+    width: 200px;
+    background: #898989;
+    border-radius: 100px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+.toggle > label:after {
+    position: absolute;
+    left: -2px;
+    top: -10px;
+    display: block;
+    width: 104px;
+    height: 104px;
+    border-radius: 100px;
+    background: #fff;
+    box-shadow: 0px 3px 3px rgba(0,0,0,0.05);
+    content: '';
+    transition: all 0.3s ease;
+}
+.toggle > label:active:after {
+    transform: scale(1.15, 0.85);
+}
+.toggle > input:checked ~ label {
+    background: #6fbeb5;
+}
+.toggle > input:checked ~ label:after {
+    left: 100px;
+    background: #179588;
+}
+.toggle > input:disabled ~ label {
+    background: #d5d5d5;
+    pointer-events: none;
+}
+.toggle > input:disabled ~ label:after {
+    background: #bcbdbc;
+}
+</style>
+EOF
+}
+
+_BOTAO(){
+echo "<center><div class='toggle'>"
+echo "<h1>${EQ[$1]}</h1>"
+echo    "<input onclick='acao$1()' type='checkbox' id='t$1' ${C[$1]}>"
+#echo    "<button onclick='acao()'>Teste</button>"
+echo    "<label for='t$1'></label>"
+echo "</div></center>"
+}
+
+_SCRIPT(){
+#cat <<EOF
+echo "<script>"
+echo "var xmlhttp;"
+echo "xmlhttp=new XMLHttpRequest();"
+for i in $(seq ${#EQ[@]}); do
+ echo "function acao$i()"
+        echo "{"
+        echo "xmlhttp.open(\"GET\",\"acao.sh?$i\", true);"
+        echo "xmlhttp.send();"
+        echo "}"
+done
+echo "</script>"
+#EOF
+}
+
+_CSS
+for i in $(seq ${#EQ[@]}); do
+ if [[ ${EQ[$i]} != "" ]]; then
+  _BOTAO $i
+ fi
+done
+#_BOTAO 2
+#_BOTAO 3
+#_BOTAO 4
+_SCRIPT
